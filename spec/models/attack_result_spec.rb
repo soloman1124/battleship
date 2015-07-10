b605d61805c27378e13d8094fecf75022285bcef
soloman.weng <solomanweng@ip-192-168-1-64.ap-southeast-2.compute.internal>
2015-07-10 12:19:32 +1000

    tabfy

--- spec/models/attack_result_spec.rb
+++ spec/models/attack_result_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 module Battleship
-  module Models
-    describe AttackResult do
-      let(:ship) { double(Ship) }
+	module Models
+		describe AttackResult do
+			let(:ship) { double(Ship) }
 
-      subject(:result) { AttackResult.new :missed, ship }
+			subject(:result) { AttackResult.new :missed, ship }
 
-      its(:position_status) { should be :missedx }
-      its(:ship_destroyed) { should be ship }
-    end
-  end
+			its(:position_status) { should be :missed }
+			its(:ship_destroyed) { should be ship }
+		end
+	end
 end
